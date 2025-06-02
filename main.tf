65e979b7f427e2fb8a6f514ff039acc5e4e5ed88
deshshep <dshepherd@bgprod.com>
2025-06-02 16:52:27 -0500

    Provision FireWall-Admin Account

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: MPL-2.0
 
 module "aft" {
-  source = "github.com/aws-ia/terraform-aws-control_tower_account_factory"
+  source                      = "github.com/aws-ia/terraform-aws-control_tower_account_factory"
   ct_management_account_id    = var.ct_management_account_id
   log_archive_account_id      = var.log_archive_account_id
   audit_account_id            = var.audit_account_id
@@ -11,8 +11,8 @@ module "aft" {
   tf_backend_secondary_region = var.tf_backend_secondary_region
 
   vcs_provider                                  = "github"
-  account_request_repo_name                     = "${var.github_username}/learn-terraform-aft-account-request"
-  account_provisioning_customizations_repo_name = "${var.github_username}/learn-terraform-aft-account-provisioning-customizations"
-  global_customizations_repo_name               = "${var.github_username}/learn-terraform-aft-global-customizations"
-  account_customizations_repo_name              = "${var.github_username}/learn-terraform-aft-account-customizations"
+  account_request_repo_name                     = "${var.github_username}/terraform-aft-account-request"
+  account_provisioning_customizations_repo_name = "${var.github_username}/terraform-aft-account-provisioning-customizations"
+  global_customizations_repo_name               = "${var.github_username}/terraform-aft-global-customizations"
+  account_customizations_repo_name              = "${var.github_username}/terraform-aft-account-customizations"
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,10 +1,10 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-ct_management_account_id    = ""
-log_archive_account_id      = ""
-audit_account_id            = ""
-aft_management_account_id   = ""
-github_username             = ""
-ct_home_region              = ""
-tf_backend_secondary_region = ""
+ct_management_account_id    = "173310184701"
+log_archive_account_id      = "422160113770"
+audit_account_id            = "797161732442"
+aft_management_account_id   = "017528860292"
+github_username             = "deshshep"
+ct_home_region              = "us-east-1"
+tf_backend_secondary_region = "us-east-2"
